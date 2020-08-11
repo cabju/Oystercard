39498798f8a76a9def0ed65f55c5ebabbd635ade
Carlo <Carlo_junior@hotmail.co.uk>
2020-08-11 13:49:52 +0100

    first commit

---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,18 @@
+require 'Oystercard'
+
+describe Oystercard do
+
+  it "Expect oystercard to respond to balance method" do
+    expect(subject).to respond_to(:balance)
+  end
+
+  it "Expects balance to be zero" do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq (0)
+  end
+
+  it "Expects oystercard to respond to 'top_up'" do
+    expect(subject).to respond_to(:top_up)
+  end
+
+end
